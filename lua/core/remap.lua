e37ccde55d20fc7106ce820e22c0fd93df6c04c1
Borgar Barland <142639383+borgaar@users.noreply.github.com>
2026-03-13 11:45:11 +0100

    added go to reference keymap

--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -10,6 +10,7 @@ end, { desc = "Collapse all except current file path" })
 
 -- LSP
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Go to definition' })
+vim.keymap.set('n', 'gr', vim.lsp.buf.references, { desc = 'Go to references' })
 vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'Open documentation' })
 vim.keymap.set('n', 'E', vim.diagnostic.open_float, { desc = 'Open diagnostics' })
 vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = 'Rename symbol' })
