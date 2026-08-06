e49a2448a36d26f653b71052cf6790d6da1e65c6
Borgar Barland <142639383+borgaar@users.noreply.github.com>
2026-08-06 09:28:39 +0200

    formatting and colorscheme

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -1,16 +1,16 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -18,93 +18,84 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 require("lazy").setup({
-  spec = {
-    -- Telescope
-    {
-      "nvim-telescope/telescope.nvim",
-      dependencies = { "nvim-lua/plenary.nvim" },
-    },
+	spec = {
+		-- Telescope
+		{
+			"nvim-telescope/telescope.nvim",
+			dependencies = { "nvim-lua/plenary.nvim" },
+		},
 
-    -- Colorscheme
-    {
-      "ellisonleao/gruvbox.nvim",
-      lazy = false,
-      priority = 1000,
-      config = function()
-        require("gruvbox").setup({
-          italic = {
-            strings = false,
-            emphasis = false,
-            comments = false,
-            operators = false,
-            folds = false,
-          },
-        })
-        vim.cmd("colorscheme gruvbox")
-      end,
-    },
+		-- Colorscheme
+		{
+			"EdenEast/nightfox.nvim",
+			lazy = false,
+			priority = 1000,
+			config = function()
+				require("nightfox").setup({})
+				vim.cmd.colorscheme("carbonfox")
+			end,
+		},
 
-    -- Formatting
-    {
-      'stevearc/conform.nvim',
-      opts = {},
-    },
+		-- Formatting
+		{
+			"stevearc/conform.nvim",
+			opts = {},
+		},
 
-    {
-        "seblyng/roslyn.nvim",
-        -- @module 'roslyn.config'
-        -- @type RoslynNvimConfig
-    },
+		{
+			"seblyng/roslyn.nvim",
+			-- @module 'roslyn.config'
+			-- @type RoslynNvimConfig
+		},
 
-    -- Diagnostics
-    {
-      "folke/trouble.nvim",
-      dependencies = { "nvim-tree/nvim-web-devicons" },
-      opts = {},
-    },
+		-- Diagnostics
+		{
+			"folke/trouble.nvim",
+			dependencies = { "nvim-tree/nvim-web-devicons" },
+			opts = {},
+		},
 
-    -- Completions
-    {
-      "hrsh7th/nvim-cmp",
-      dependencies = { "hrsh7th/cmp-nvim-lsp" }
-    },
+		-- Completions
+		{
+			"hrsh7th/nvim-cmp",
+			dependencies = { "hrsh7th/cmp-nvim-lsp" },
+		},
 
-    -- Status line
-    {
-      "nvim-lualine/lualine.nvim",
-      dependencies = { "nvim-tree/nvim-web-devicons" },
-      opts = {
-        options = {
-          theme = "gruvbox",
-        },
-      },
-    },
+		-- Status line
+		{
+			"nvim-lualine/lualine.nvim",
+			dependencies = { "nvim-tree/nvim-web-devicons" },
+			opts = {
+				options = {
+					theme = "carbonfox",
+				},
+			},
+		},
 
-    -- Smart splits
-    { "mrjones2014/smart-splits.nvim", opts = {} },
+		-- Smart splits
+		{ "mrjones2014/smart-splits.nvim", opts = {} },
 
-    -- Floating terminal
-    { "voldikss/vim-floaterm" },
+		-- Floating terminal
+		{ "voldikss/vim-floaterm" },
 
-    -- Autopairs
-    {
-      "windwp/nvim-autopairs",
-      event = "InsertEnter",
-      opts = {},
-    },
+		-- Autopairs
+		{
+			"windwp/nvim-autopairs",
+			event = "InsertEnter",
+			opts = {},
+		},
 
-    -- Surround
-    {
-      "kylechui/nvim-surround",
-      version = "*",
-      event = "VeryLazy",
-      config = function()
-        require("nvim-surround").setup()
-      end,
-    },
+		-- Surround
+		{
+			"kylechui/nvim-surround",
+			version = "*",
+			event = "VeryLazy",
+			config = function()
+				require("nvim-surround").setup()
+			end,
+		},
+	},
 
-  },
-
-  install = { colorscheme = { "gruvbox", "habamax" } },
-  checker = { enabled = true },
+	install = { missing = true },
+	checker = { enabled = true },
 })
